ba12985d692f5fa966f52d9b86778dcb6551725b
3nigma-tik <cole.blackwood@icloud.com>
2021-10-06 09:17:03 -0700

    initial commit of robo vs dino uml

---
+++ robots.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self, name):
+        self.name = ""
+        self.health = 0
+        self.weapon = []
+        return
+
+    def attack(self, dinosaur):
+        pass
